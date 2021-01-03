278f9c98c2520daa27cddcf65fc13186714e699f
jclot <julian.clot@iem.ed.cr>
2021-01-03 13:40:20 -0600

    Change to main executable file

--- src/gitinit/__init__.py
+++ src/gitinit/__init__.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
-from pkg_resources import DistributionNotFound, get_distribution
+# Author: Julian Clot Cordoba
 
-try:
-    # Change here if project is renamed and does not equal the package name
-    dist_name = __name__
-    __version__ = get_distribution(dist_name).version
-except DistributionNotFound:
-    __version__ = "unknown"
-finally:
-    del get_distribution, DistributionNotFound
+from window import main_window
+
+def __main__():
+    main_window()
+
+__main__()
+
+
