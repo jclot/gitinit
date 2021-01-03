1cbbc5dee192240d628712bdeef106686ce35b51
jclot <julian.clot@iem.ed.cr>
2021-01-03 13:42:01 -0600

    The skeleton of the app

--- src/gitinit/skeleton.py
+++ src/gitinit/skeleton.py
@@ -1,14 +1,4 @@
 from tkinter import *
 
+    def navbar():
 
-def window_app():
-
-    window = Tk()
-    window.title("Welcome to LikeGeeks app")
-    window.mainloop()
-
-def __main__():
-
-    window_app()
-
-__main__()
