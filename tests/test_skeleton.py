4a9d8b17ff49154f685bb1e6ce92288d3f093b8d
Clot <58281152+jclot@users.noreply.github.com>
2021-01-07 17:10:26 -0600

    Create a test python file (Not Finished)

--- tests/test_skeleton.py
+++ tests/test_skeleton.py
@@ -1,17 +1,13 @@
 # -*- coding: utf-8 -*-
 
 import pytest
-
-from gitinit.skeleton import fib
+from gitinit.skeleton import choose_dir
 
 __author__ = "jclot"
 __copyright__ = "jclot"
 __license__ = "mit"
 
+def test_dir():
+    assert choose_dir()
 
-def test_fib():
-    assert fib(1) == 1
-    assert fib(2) == 1
-    assert fib(7) == 13
-    with pytest.raises(AssertionError):
-        fib(-10)
+
